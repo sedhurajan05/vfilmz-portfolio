281d3f861b8c923ebd2116e84754985b07e0beb3
Sedhurajan S <sedhurajan.s@ba.com>
2026-08-28 22:55:06 +0100

    fix: cloudinary URL using public_id only

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ async function fetchFromCloudinary(cat) {
   if (!res.ok) return [];
   const data = await res.json();
   return data.resources.map(r => ({
-    src: `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/q_auto,f_auto/${r.asset_folder}/${r.public_id}.${r.format}`,
+    src: `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/q_auto,f_auto/${r.public_id}`,
     title: r.public_id.replace(/[-_]/g, ' '),
     cat,
     h: r.height > r.width ? 'tall' : 'wide'
